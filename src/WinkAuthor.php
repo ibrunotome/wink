6a5a4653b44c40fadae4fb99785ea28950de19c4
Bruno Tomé <ibrunotome@gmail.com>
2024-07-07 18:43:43 -0300

    refactor: implement missing interface method

--- src/WinkAuthor.php
+++ src/WinkAuthor.php
@@ -121,6 +121,16 @@ class WinkAuthor extends AbstractWinkModel implements Authenticatable
     }
 
     /**
+     * Get the name of the password attribute for the user.
+     *
+     * @return string
+     */
+    public function getAuthPasswordName()
+    {
+        return 'password';
+    }
+
+    /**
      * Get the token value for the "remember me" session.
      *
      * @return string|null
